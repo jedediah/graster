2ed628b160ab0b60e5e0300436230893a3ce99ad
Jedediah Smith <jed@winky.hacklab.to>
2009-04-08 13:00:43 -0400

    greyscale experiments

--- graster.rb
+++ graster.rb
@@ -2,6 +2,8 @@
 
 require 'yaml'
 
+ROOT2 = Math.sqrt(2)
+
 begin
   filename = ARGV[0] or raise
   $width = ARGV[1].to_i
@@ -11,85 +13,114 @@ rescue
   exit 1
 end
 
-config = YAML.load_file("graster.yml") or raise "can't find graster.yml"
-puts "(#{config.inspect})"
+$config = YAML.load_file("graster.yml") or raise "can't find graster.yml"
 
-$threshold = config["threshold"].to_i
-$zon = config["zon"].to_f
-$zoff = config["zoff"].to_f
-$step = 1.0/config["dpi"].to_i
-$linestep = config["linestep"].to_i
-$leftmargin = config["leftmargin"].to_f
-$bottommargin = config["bottommargin"].to_f
-$feed = config["feed"].to_f
-$whitefeed = config["whitefeed"].to_f
-$greyscale = config["greyscale"]
+$threshold = $config["threshold"].to_i
+$negative = $config["negative"]
+$zon = $config["zon"].to_f
+$zoff = $config["zoff"].to_f
+$zdelta = ($zoff-$zon).abs
+$step = 1.0/$config["dpi"].to_i
+$linestep = $config["linestep"].to_i
+$leftmargin = $config["leftmargin"].to_f
+$bottommargin = $config["bottommargin"].to_f
+$feed = $config["feed"].to_f
+$whitefeed = $config["whitefeed"].to_f
+$greyscale = $config["greyscale"]
 
-def draw_row xorig, yorig, pix, step
+
+### monochrome rastering
+
+def draw_span x1, x2
+  if x1 < x2
+    "G0 X#{x1}\n" +
+    "G1 X#{x1+$zdelta} Z#{$zon} F#{$feed*ROOT2}\n" +
+    "G1 X#{x2} F#{$feed}\n" +
+    "G1 X#{x2+$zdelta} Z#{$zoff} F#{$feed*ROOT2}\n"
+  else
+    "G0 X#{x1+$zdelta}\n" +
+    "G1 X#{x1} Z#{$zon} F#{$feed*ROOT2}\n" +
+    "G1 X#{x2+$zdelta} F#{$feed}\n" +
+    "G1 X#{x2} Z#{$zoff} F#{$feed*ROOT2}\n"
+  end
+end
+
+def draw? pix, param
+  if $greyscale
+    pix == param
+  elsif $negative
+    pix > $threshold
+  else
+    pix < $threshold
+  end
+end
+
+def draw_row xorig, yorig, pix, param, step, margin
   z = false
-  yflag = false
+  x1 = nil
 
-  (pix.size+1).times do |i|
-    if !z && i < pix.size && pix[i].ord < $threshold
+  #puts "(#{pix.inspect})"
+  res = "G0 Y#{yorig} X#{xorig+margin}\n"
+
+  pix.size.times do |i|
+    if !z && draw?(pix[i],param)
       z = true
-      print(if yflag
-              "G0"
-            else
-              yflag = true
-              "G0 Y#{yorig}"
-            end + " X#{xorig + i*step}\nG1 X#{xorig + (i+1)*step} Z#{$zon}\n")
-    elsif z && (i == pix.size || pix[i].ord >= $threshold)
+      x1 = xorig + i*step
+    elsif z && !draw?(pix[i],param)
       z = false
-      print "G1 X#{xorig + i*step}\nG1 X#{xorig + (i+1)*step} Z#{$zoff}\n"
+      res << draw_span(x1, xorig + i*step)
     end
   end
+
+  z and res << draw_span(x1, xorig + pix.size*step)
+  res
 end
 
-def pix_to_feed pix
-  $feed + ($whitefeed - $feed) * (pix.to_f/255.0)**2
-end
 
-def draw_greyscale_row xorig, yorig, pix, step, margin
-  print "G0 Z#{$zoff} X#{xorig+margin} Y#{yorig}\nG1 X#{xorig-step} F#{$feed}\n"
-  pix.size.times do |i|
-    print(if i == 0
-            "G1 Z#{$zon}"
-          elsif i == pix.size-1
-            "G1 Z#{$zoff}"
-          else
-            "G1"
-          end + " X#{xorig + i*step} F#{$feed + pix[i].ord.to_f*($whitefeed-$feed)/255.0}\n")
-  end
-end
-
-File.open(filename,"rb") do |raw|
-  print "M3\nG0 Z#{$zoff}\nF#{$feed}\n"
+def render raw, param
+  res = "(#{$config.inspect})\nM3\nG0 Z#{$zoff}\nF#{$feed}\n"
   forward = true
 
   $height.times do |y|
-    row = raw.read($width)
-    row.size == $width or raise "premature EOF"
+    row = raw[y*$width...(y+1)*$width]
 
     if y % $linestep == 0
       if forward
-        if $greyscale
-          draw_greyscale_row $leftmargin, $bottommargin + ($height-y)*$step, row, $step, -$leftmargin/2.0
-        else
-          draw_row $leftmargin, $bottommargin + ($height-y)*$step, row, $step
-        end
+        res += draw_row($leftmargin,
+                        $bottommargin + ($height-y)*$step,
+                        row,
+                        param,
+                        $step,
+                        -$leftmargin/2.0)
         forward = false
       else
-        if $greyscale
-          draw_greyscale_row $leftmargin + $width*$step, $bottommargin + ($height-y)*$step, row.reverse, -$step, $leftmargin/2.0
-        else
-          draw_row $leftmargin + $width*$step, $bottommargin + ($height-y)*$step, row.reverse, -$step
-        end
+        res += draw_row($leftmargin + $width*$step,
+                        $bottommargin + ($height-y)*$step,
+                        row.reverse,
+                        param,
+                        -$step,
+                        $leftmargin/2.0)
         forward = true
       end
-      print "\n"
+      res << "\n"
     end
   end
 
-  print "M2\n"
+  res + "M2\n"
 end
 
+
+### main loop
+
+raw = File.open(filename,"rb") {|io| io.bytes.to_a }
+raw.size == $width*$height or raise "file size does not match image dimensions"
+
+if $greyscale
+  raw.uniq.each do |plane|
+    File.open("#{filename}.#{'%02x'%plane}.ngc","w") do |io|
+      io.write render(raw,plane)
+    end
+  end
+else
+  print render(raw,$threshold)
+end
